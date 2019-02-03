1a23fe37290edfe3837dc33be014ed3a3e116a0a
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2019-02-03 15:25:03 +0600

    Update pyautogui.lock.py

--- pyautogui.lock.py
+++ pyautogui.lock.py
@@ -1,15 +1,32 @@
 """
-pyautogui automatic pc lock code.
+Seek user permission before locking the pc.
+Version2 of pyautogui.lock.py Code.
 by Ashraf Minhaj.
-find me- ashraf_minhaj@yahoo.com
+find me at ashraf_minhaj@yahoo.com
+tutorial- ashrafminhajfb.blogspot.com
 """
 
-import pyautogui  #import pyautogui library
-from time import sleep  #import sleep - for the delay
+"""
+Change the sleep(time) / delay time as per your system.
+It is set so that the code doesn't run faster than the 
+Computer system
+"""
+import pyautogui   #import pyautogui 
+from time import sleep  #import sleep
 
-pyautogui.hotkey('win', 'r') #windows_key + Run 
+#seek user permisssion before locking
+x = pyautogui.confirm("Do you want to lock the screen?") 
 
-pyautogui.typewrite("cmd\n")  #cmd to open command prompt
-sleep(0.500)      #500 milisec delay
-#write the code then press enter('\n') thus pc will auto-lock
-pyautogui.typewrite("rundll32.exe user32.dll, LockWorkStation\n") 
+#check if user clicked Ok or Cancel
+if x == "OK":               #if User pressed OK
+    print("locking the pc")
+
+    #execute locking code
+    pyautogui.hotkey('win', 'r')  #type win+r (run) combo key
+    sleep(0.01)  #a bit delay
+    pyautogui.typewrite("cmd\n") #write cmd(command prompt) '\n' for 'ENTER' command
+    sleep(0.500)  #delay so that the code doesn't run faster than the compluter
+    pyautogui.typewrite("rundll32.exe user32.dll, LockWorkStation\n") #type and hit 'ENTER' in the command prompt
+ 
+if x == "Cancel":  #if Cancel do nothing
+    print("screen lock cancelled")
